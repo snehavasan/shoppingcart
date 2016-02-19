4cf5ba6b5b41647b54d8ffa560fc1940fe9d78d1
snehavasan <snehav@utexas.edu>
2016-02-19 17:01:20 -0600

    with testcases

--- A3/src/Assignment3/Driver.java
+++ A3/src/Assignment3/Driver.java
@@ -1,12 +1,23 @@
 package Assignment3;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
+
 
 public class Driver 
 	{
 
 	  public static void main(String[] args) 
 	  {
+			if (args.length != 1) 
+			{
+				System.err.println ("Error: Incorrect number of command line arguments");
+				System.exit(-1);
+			}
+			processLinesInFile (args[0]);
 		// TODO Auto-generated method stub
 		
 		//Open file; file name specified in args (command line)
@@ -28,5 +39,41 @@ public class Driver
 			// of Grocery, it will invoke the calculatePrice () method defined in Grocery.
 		}		
 	  }
+	  
+		public static void processLinesInFile (String filename) 
+		{ 
+
+			Driver processor = new Driver(); 
+			try 
+			{
+				FileReader freader = new FileReader(filename);
+				BufferedReader reader = new BufferedReader(freader);
+				
+				for (String s = reader.readLine(); s != null; s = reader.readLine()) 
+				{
+					String output = processor.process(s);
+					System.out.println(output);
+				}
+			} 
+			catch (FileNotFoundException e) 
+			{
+				System.err.println ("Error: File not found. Exiting...");
+				e.printStackTrace();
+				System.exit(-1);
+			} catch (IOException e) 
+			{
+				System.err.println ("Error: IO exception. Exiting...");
+				e.printStackTrace();
+				System.exit(-1);
+			}
+		}
+		// Process the strings in this 
+		public String process (String inputString){
+			String output = new String(inputString);
+			return output;
+			
+			
+		}
+		
 
 }
